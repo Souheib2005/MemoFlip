b6a82093c78fc885e00e0abd623fdc1fe1d15330
Souheib2005 <souheibalahdal2005@gmail.com>
2025-06-20 16:15:43 -0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/memoflip/', // <-- Your repo name here
+  // base: '/memoflip/',  ← Comment this out for local dev
   plugins: [react()],
 })
